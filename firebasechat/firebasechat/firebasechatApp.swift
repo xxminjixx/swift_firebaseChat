1d745a00ea2134ce70fa38a4b3c60b2e10818bdc
minji choi <so8376@gmail.com>
2022-08-06 19:06:04 +0900

    init

---
+++ firebasechat/firebasechat/firebasechatApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebasechatApp.swift
+//  firebasechat
+//
+//  Created by 최민지 on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct firebasechatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
